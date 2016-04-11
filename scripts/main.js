7071f4a833137e0dbb563d4b3b944ca8b39a5d9b
gregkorte <gryphionsroost@yahoo.com>
2016-04-11 11:30:49 -0500

    Starting refactor. Geolocation working.

--- scripts/main.js
+++ scripts/main.js
@@ -1,89 +1,102 @@
-var url = 'http://api.wunderground.com/api/1e1243f5d9365714/forecast10day/q/37217.json'
+var url = 'http://api.wunderground.com/api/1e1243f5d9365714/geolookup/q/'
 
-function getJSONP(url, cbName){
+//10dayForecast
+//http://api.wunderground.com/api/1e1243f5d9365714/geolookup/q/37.776289,-122.395234.json
+
+function getJSONP(url, lat, lon, request){
 	var $script = document.createElement('script');
-	$script.src = url + '?callback=' + cbName;
+	$script.src = url + lat + ',' + lon + '?callback=' + request;
+    console.log($script.src)
 	document.body.appendChild($script);
 }
 
-function cbName_myAwesomeFunction(data){
-	var $target = document.querySelector("section");
+function getJSONZip(url, request){
+    console.log('getJSONZip running');
+    var $script = document.createElement('script');
+    console.log($script);
+    var zipCodeInput = document.querySelector('.zipCodeInput').value;
+    console.log(zipCodeInput);
+    $script.src = url + zipCodeInput + '.json' + '?callback=' + request;
+    document.body.appendChild($script);
+}
 
-// function createDivTag(data.forecast.simpleforecast.forecastday[i]){
-//     var docFragment = document.createDocumentFragment();
-      
-//       var $div = document.createElement('div');
-//       div$setAttribute('class', 'dayWidget');
-//       docFragment.appendChild($div);
+function showWeatherData(jsondata){
+	var $target = document.querySelector('section');
+    var $button = document.querySelector('.button');
 
-//       return docFragment;
-// }
-
-  var docFragment = document.createDocumentFragment();
-  var $div = document.createElement('div');
-  $div.setAttribute('class', 'dayWidget');
-  docFragment.appendChild($div);
-  $target.appendChild($div);
-  
+  var docFragment = document.createDocumentFragment()
   for(var i = 0; i < 5; i++){
-    var forecastDay = data.forecast.simpleforecast.forecastday[i];
+    var forecastDay = jsondata.forecast.simpleforecast.forecastday[i];
 
-    var forecastDate = data.forecast.simpleforecast.forecastday[i].date;
+    var forecastDate = forecastDay.date;
     var month = forecastDate.monthname_short;
     var day = forecastDate.day;
     var year = forecastDate.year;
-    var high = data.forecast.simpleforecast.forecastday[i].high;
-    var low = data.forecast.simpleforecast.forecastday[i].low;
-    var conditions = data.forecast.simpleforecast.forecastday[i].conditions;
-    var icon = data.forecast.simpleforecast.forecastday[i].icon;
-    var icon_url = data.forecast.simpleforecast.forecastday[i].icon_url;
+    var high = forecastDay.high;
+    var low = forecastDay.low;
+    var conditions = forecastDay.conditions;
+    var icon = forecastDay.icon;
+    var icon_url = forecastDay.icon_url;
 
-    var $forecastDayp = document.createElement('h2');    
+    var $div = document.createElement('div');
+    $div.setAttribute('class', 'dayWidget');
+    docFragment.appendChild($div);
+    $target.appendChild($div);
+
+    var $forecastDayp = document.createElement('h2');
     $forecastDayp.textContent = forecastDay.date.weekday;
     docFragment.appendChild($forecastDayp);
-    $target.appendChild($forecastDayp);
+    $div.appendChild($forecastDayp);
 
     var $dateSpan = document.createElement('span');
     $dateSpan.textContent = month + ". " + day + ", " + year;
     $dateSpan.setAttribute('class', 'dateSpan');
     docFragment.appendChild($dateSpan);
-    $target.appendChild($dateSpan);
+    $div.appendChild($dateSpan);
 
-    var $highDiv = document.createElement('div');    
-    $highDiv.textContent = high.fahrenheit + "&deg;";
+    var $highDiv = document.createElement('div');
+    $highDiv.textContent = high.fahrenheit + "\u00b0";
     $highDiv.setAttribute('class', 'highDiv');
     docFragment.appendChild($highDiv);
-    $target.appendChild($highDiv);
+    $div.appendChild($highDiv);
 
-    var $lowDiv = document.createElement('div');    
-    $lowDiv.textContent = low.fahrenheit + "&deg;";
+    var $lowDiv = document.createElement('div');
+    $lowDiv.textContent = low.fahrenheit + "\u00b0";
     $lowDiv.setAttribute('class', 'lowDiv');
     docFragment.appendChild($lowDiv);
-    $target.appendChild($lowDiv);
+    $div.appendChild($lowDiv);
 
-    var $iconimg = document.createElement('img');    
+    var $iconimg = document.createElement('img');
     $iconimg.textContent = icon;
     $iconimg.setAttribute('class', 'conditionsImage');
     $iconimg.setAttribute('src', icon_url);
     $iconimg.setAttribute('alt', icon);
     docFragment.appendChild($iconimg);
-    $target.appendChild($iconimg);    
+    $div.appendChild($iconimg);
 
-    var $conditionsSpan = document.createElement('span');    
+    var $conditionsSpan = document.createElement('span');
     $conditionsSpan.textContent = conditions;
     $conditionsSpan.setAttribute('class', 'conditionsSpan');
     docFragment.appendChild($conditionsSpan);
-    $target.appendChild($conditionsSpan);
-
-  }   
+    $div.appendChild($conditionsSpan);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-  getJSONP(url, 'cbName_myAwesomeFunction');
-});
+    var lat;
+    var lon;
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude)
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+    });
+  getJSONP(url, lat, lon, 'showWeatherData');
 
-//Iterate through data.forecast.simpleforecast.forecastday array
-//Collect [0-4]
-//Append data to DOM
-  //Create ul
-  //Append with li
+var $button = document.querySelector('.button');
+var $target = document.querySelector('section');
+  $button.addEventListener('click', function(){
+    $target.empty();
+  getJSONZip(url, 'showWeatherData');
+  $div.appendChild(docFragment);
+  });
+});
